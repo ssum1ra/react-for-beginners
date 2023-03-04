15268e1fc95e204e65bd03d54a4dea0091e5b26a
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2023-03-04 19:58:29 +0900

    initial version

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
+  const [movie, setMovie] = useState([]);
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json.data.movie);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      <img src={movie.medium_cover_image} />
+      <div>
+        <h2>{movie.title}</h2>
+        <h3>{movie.year}</h3>
+        <p>{movie.description_full}</p>
+        <ul>{movie.genres && movie.genres.map((g) => <li key={g}>{g}</li>)}</ul>
+      </div>
+    </div>
+  );
 }
 export default Detail;
